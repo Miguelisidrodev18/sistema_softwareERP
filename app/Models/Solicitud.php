9d5950600cc110e3d3441c45305c27587ddbea53
Miguel Isidro <74093841@continental.edu.pe>
2026-06-01 07:25:40 -0500

    fix: tabla solicitudes nombre explicito

--- app/Models/Solicitud.php
+++ app/Models/Solicitud.php
@@ -10,6 +10,8 @@ class Solicitud extends Model
 {
     use SoftDeletes;
 
+    protected $table = 'solicitudes';
+
     protected $fillable = [
         'user_id', 'tipo', 'titulo', 'descripcion', 'monto',
         'estado', 'respuesta_admin', 'atendido_por', 'atendido_at',
